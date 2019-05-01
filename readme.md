7095e14ccc9b97d893f03a437a5b70e504dde7d8
Georgy Marchuk <gmarcuk@gmail.com>
2019-05-02 00:34:51 +0200

    init

---
+++ readme.md
@@ -0,0 +1,35 @@
+# Swup Debug plugin
+Debug plugin adds some useful functionality to the swup for development purposes. 
+Mainly, it outputs all the events in a console as they happen, which can be useful for debugging. 
+
+Plugin also rewrites swups `log` method, so any output provided by plugins is also visible. 
+
+Plugin also tries to detect some common mistakes, and outputs a suggestions into a console.   
+
+## Instalation
+This plugin can be installed with npm
+
+```bash
+npm install @swup/ga-plugin
+```
+
+and included with import
+
+```shell
+import SwupDebugPlugin from '@swup/debug-plugin';
+```
+
+or included from the dist folder
+
+```html
+<script src="./dist/SwupDebugPlugin.js"></script>
+```
+
+## Usage
+To run this plugin, include an instance in the swup options.
+
+```javascript
+const swup = new Swup({
+  plugins: [new SwupDebugPlugin()]
+});
+```
---
+++ src/index.js
@@ -0,0 +1,70 @@
+import Plugin from '@swup/plugin';
+
+export default class DebugPlugin extends Plugin {
+    name = "SwupDebugPlugin";
+
+    mount() {
+        const swup = this.swup;
+
+        // set non-empty log method of swup
+        swup.log = this.log;
+
+        // set swup instance as a global variable swup
+        window.swup = swup;
+
+        // make events appear in console
+        swup._triggerEvent = swup.triggerEvent;
+        swup.triggerEvent = this.triggerEvent;
+
+        // detect relative links not starting with / or #
+        const potentiallyWrongLinksSelector =  'a[href]:not([href^="' + window.location.origin + '"]):not([href^="/"]):not([href^="http"]):not([href^="/"]):not([href^="?"]):not([href^="#"])';
+
+        swup.on('pageView', () => {
+            if (document.querySelectorAll(potentiallyWrongLinksSelector).length) {
+                const error = 'It seems there are some links with a href attribute not starting with "#", "/" or current domain, which is potentially a problem.';
+                console.warn(`DEBUG PLUGIN: ${error}`, document.querySelectorAll(potentiallyWrongLinksSelector));
+            }
+            if (document.querySelectorAll(potentiallyWrongLinksSelector).length) {
+                const error = 'It seems there are some links with a href attribute not starting with "#", "/" or current domain, which is potentially a problem.';
+                console.warn(`DEBUG PLUGIN: ${error}`, document.querySelectorAll(potentiallyWrongLinksSelector));
+            }
+        });
+    }
+
+    unmount() {
+        this.swup.log = () => {};
+        this.swup.triggerEvent = this.swup._triggerEvent;
+    }
+
+    triggerEvent = (eventName, originalEvent) => {
+        if (originalEvent) {
+            console.groupCollapsed('%cswup:' + '%c' + eventName, 'color: #343434', 'color: #009ACD');
+            console.log(originalEvent);
+            console.groupEnd();
+        } else {
+            console.log('%cswup:' + '%c' + eventName, 'color: #343434', 'color: #009ACD');
+        }
+
+        this.swup._triggerEvent(eventName, originalEvent);
+    };
+
+    log = (str, object) => {
+        if (object) {
+            console.groupCollapsed(str);
+            for (let key in object) {
+                console.log(object[key]);
+            }
+            console.groupEnd();
+        } else {
+            console.log(str + '%c', 'color: #009ACD');
+        }
+    };
+
+    debugLog = (log, type) => {
+        if (type === 'error') {
+            console.error(`DEBUG PLUGIN: ${log}`);
+        } else {
+            console.warn(`DEBUG PLUGIN: ${log}`);
+        }
+    };
+}
